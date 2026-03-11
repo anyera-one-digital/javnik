4cf3d4b09ff9d519e9d39748fba096583a0b6fc2
Dmitry Kovalev <pzbhabbiu@gmail.com>
2026-03-11 19:43:31 +0300

    Обновил зависимости бекенда.

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status, generics, permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from .models import SpecialtyCategory, Specialty
+from .models import SpecialtyCategory
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import get_user_model
 from django.core.cache import cache
